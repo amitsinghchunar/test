fdc6615c9f999db59226eddc3cc4002794930065
amit <amitsingh0487@gmail.com>
2020-03-08 22:43:36 +0530

    This code is for for looping concept

---
+++ forLoop.py
@@ -0,0 +1,20 @@
+my_string = "abcdabcd"
+for i in my_string:
+    if i == "a":
+        print(i.upper(), end=' ')
+    else:
+        print(i, end=' ')
+
+print("*" * 50)
+
+school = {"name": "Amit", "standard": 10, "address": "UP"}
+for x,y in school.items():
+    print(x)
+    print(y)
+
+
+
+
+
+
+
