f8b58411d661f5c8edc6fabeab1349279b4a956e
amit <amitsingh0487@gmail.com>
2020-03-08 22:13:11 +0530

    Modified the code for breakAndContinue block

--- breakAndContinue.py
+++ breakAndContinue.py
@@ -2,9 +2,16 @@ x = 0
 while x < 11:
     print("The value of x is" + " " + str(x))
     x = x + 1
+    print("now lets check the condition")
 
-    if x == 5:
-        continue
+    while x < 4:
+        print("The value of x is" + " " + str(x))
+        x = x + 1
+        if x == 4:
+            break
+
+
+
     print("This is a good example")
     print("*" * 20)
 
